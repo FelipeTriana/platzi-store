4dd400da97d60db451bd42d7022efea37248d81a
FelipeTriana <felipe.trianag@udea.edu.co>
2021-05-14 17:08:44 -0500

    Informacion sobre status codes y response con express

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,45 +1,64 @@
-import { Controller, Get, Post, Query, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
+
+import { Response } from 'express';
 
 @Controller('products')
 export class ProductsController {
-  /**
-   * Para obtener parámetros de la URL se puede usar el decorador @Params
-   */
-
-  /*Esta ruta tendria problemas con la ruta dinamica que se encuentra inmediatamente despues, si tenemos rutas parecidas las NO dinamicas deben ir ANTES que 
-   las dinamicas. Si esta ruta estuviera debajo de products/:productId nunca podriamos acceder a ella*/
-  @Get('filter')
-  getProductFilter() {
-    return `yo soy un filter`;
-  }
-
-  @Get(':productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-
-   /**
-   * Parametros tipo Query
-   * Se usan para no enviar grandes conjuntos de parametros en un endpoint.
-   * La lista de query params empieza con un '?' y se concatenan entre si con un '&': api.example.com/products?region=USA&brand=adidas&sort=asc
-   * Ejemplo: Puede servir para un filtro donde obtengamos todos los productos de una region, una marca y un orden especifico 
-   */
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
-    //Acceder por ejemplo a: http://localhost:3000/products?limit=1000&offset=25&brand=ardidas     no importa el orden en que se envien los parametros
+    return {
+      message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    };
   }
 
-  @Post() // 👈 New decorator
+  @Get('filter')
+  @HttpCode(HttpStatus.ACCEPTED)                 //Cuando hagamos la solicitud en postman el statuscode deberia ser un 202
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+
+  @Get(':productId')
+  getOne(@Res() response: Response, @Param('productId') productId: string) {  //OJO: AL DECLARAR UN RESPONSE EL RETURN DEJA DE FUNCIONAR
+    response.status(200).send({                                               //LE ESTAMOS DICIENDO A NESTJS QUE NOSOTROS NOS 
+                                                                              //ENCARGAREMOS DE LA RESPUESTA
+      message: `product ${productId}`,
+    });
+  }
+
+  @Post()
   create(@Body() payload: any) {
     return {
       message: 'accion de crear',
       payload,
     };
   }
-  
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
+  }
 }
