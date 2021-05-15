6ec34d0e3693dd1e1323f2efa9042a3552023452
FelipeTriana <felipe.trianag@udea.edu.co>
2021-05-15 06:43:49 -0500

    Validacion de DTO de forma robusta

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,21 +1,44 @@
 /**
- * Los data transfer object sirven para validar grupos de datos de trasnferencia.
- * No es buen patron combinarlo con entities porque en ocasiones necesitamos atributos agregados en las transferencia de informacion
- * que no necesariamente estan insertados en la base de datos
+ * Instalar: npm i class-validator class-transformer @nestjs/mapped-types
  */
 
-export class CreateProductDto {
-    readonly name: string;          //readonly: solo sera tipo lectura. Quiere decir que el atributo no puede ser modificado
+import {
+    IsString,
+    IsNumber,
+    IsUrl,
+    IsNotEmpty,
+    IsPositive,
+  } from 'class-validator';  //Para validar los tipos de los atributos del DTO
+
+  import { PartialType } from '@nestjs/mapped-types';  //Nos ayuda a reutilizar codigo extendendiendo clases que ya tenemos
+  
+  export class CreateProductDto {
+    @IsString()                   //Estos decoradores nos avisaran si enviamos en el body algun dato que no concuerde con el del atributo
+    @IsNotEmpty()
+    readonly name: string;
+  
+    @IsString()
+    @IsNotEmpty()
     readonly description: string;
+  
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
     readonly price: number;
+  
+    @IsNumber()
+    @IsNotEmpty()
+    @IsPositive()
     readonly stock: number;
+  
+    @IsUrl()
+    @IsNotEmpty()
     readonly image: string;
   }
   
-  export class UpdateProductDto {
-    readonly name?: string;
-    readonly description?: string;
-    readonly price?: number;
-    readonly stock?: number;
-    readonly image?: string;
-  }
+  /**
+   * Aqui eliminamos la informacion de la clase DTO: UpdateProductDTO.
+   * En su lugar usamos PartialType que nos permite reutilizar nuestro DTO base pero pone todos los atributos como OPCIONALES
+   * Esto es lo mismo que tenemos en la rama dataTransfersObject pero reutilizando el codigo del DTO CreateProductDto
+   */
+  export class UpdateProductDto extends PartialType(CreateProductDto) {}
